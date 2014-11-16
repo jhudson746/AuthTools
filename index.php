0b9adc789e69d4b4ec5ca8c1e349b204673232ce
unknown <josh@josh7-PC.flightlinegroup.com>
2014-11-16 15:14:27 -0500

    Added description and finished initial config.
    The initial introduction stuff has been placed into the project.  Ready to
    merge changes back on to develop.

--- index.php
+++ index.php
@@ -2,8 +2,25 @@
 <html>
 	<head>
 		<title>Welcome to AuthTools</title>
+		<link rel='stylesheet' type='text/css' href='css/main.css' />
 	</head>
 	<body>
 		<h1>Welcome to AuthTools</h1>
+		<p>
+			Welcome to AuthTools!  This suite of libraries are designed to
+			facilitate building an a secure web app.  You can use these
+			libraries to create password protected, session based access to your
+			web content.
+		</p>
+		<h2>Requirements</h2>
+		<ol>
+			<li>A working knowledge of PHP</li>
+			<li>A working knowledge of HTML</li>
+			<li>A working knowledge of Javascript and modern libaries (such as JQuery)</li>
+			<li>A web server or host service</li>
+			<li>PHP 5+</li>
+			<li>MySQL</li>
+			<li>A willingness to learn and have fun</li>
+		</ol>
 	</body>
 </html>
