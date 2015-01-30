4ff9110a3aeef8b86e4d77a5ad884a57765b9095
Josh Hudson <jhudson746@gmail.com>
2015-01-30 17:09:54 -0500

    Added JQuery Support
    Added JQuery Support and a skeleton installer page.

---
+++ setup/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Auth Tools Installer</title>
+		<link rel="stylesheet" type="text/css" href="../css/main.css" />
+		<script type='text/javascript' src='../js/jquery-1.11.2.min.js'></script>
+		<script type='text/javascript'>
+			$(document).ready(function () {
+				// Inintilize andy Javascript things here.
+			});
+		</script>
+	</head>
+	<body>
+		Testing...
+	</body>
+</html>
